7a58a6b4d3c99d2e157cc449cbaffe7bd6777d63
VitorOM2 <viktormarque2053@gmail.com>
2026-08-04 12:10:21 -0300

    Corrige problemas no DAO de usuário

--- src/dao/UsuarioDAO.java
+++ src/dao/UsuarioDAO.java
@@ -1,9 +1,13 @@
 package dao;
 
 import connection.ConnectionFactory;
-import model.*;
+import model.Usuario;
 
-import java.sql.*;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -14,44 +18,45 @@ public class UsuarioDAO implements InterfaceDAO<Usuario> {
     @Override
     public void salvar(Usuario usuario) {
 
-        String sql =
-                """
+        String sql = """
                 INSERT INTO usuario
-                (nome,email,senha,departamento_id)
-                VALUES (?,?,?,?)
+                    (nome, email, senha, departamento_id)
+                VALUES (?, ?, ?, ?)
                 """;
 
-        try (ConnectionFactory conn = ConnectionFactoryFactory.getConnectionFactory();
-             PreparedStatement statement = conn.prepareStatement(sql)) {
+        try (
+                Connection connection = ConnectionFactory.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql)
+        ) {
 
-                statement.setString(1, usuario.getNome());
-                statement.setString(2, usuario.getEmail());
-                statement.setString(3, usuario.getSenha());
-                statement.setInt(4, usuario.getDepartamento().getId());
+            statement.setString(1, usuario.getNome());
+            statement.setString(2, usuario.getEmail());
+            statement.setString(3, usuario.getSenha());
+            statement.setInt(4, usuario.getDepartamento().getId());
 
-                statement.executeUpdate();
+            statement.executeUpdate();
 
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Erro ao salvar usuário.", e);
         }
-
     }
 
     @Override
     public void atualizar(Usuario usuario) {
 
-        String sql =
-                """
+        String sql = """
                 UPDATE usuario
-                SET nome=?,
-                    email=?,
-                    senha=?,
-                    departamento_id=?
-                WHERE id=?
+                SET nome = ?,
+                    email = ?,
+                    senha = ?,
+                    departamento_id = ?
+                WHERE id = ?
                 """;
 
-        try (ConnectionFactory conn = conn.getConnectionFactory();
-             PreparedStatement statement = conn.prepareStatement(sql)) {
+        try (
+                Connection connection = ConnectionFactory.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql)
+        ) {
 
             statement.setString(1, usuario.getNome());
             statement.setString(2, usuario.getEmail());
@@ -62,59 +67,52 @@ public class UsuarioDAO implements InterfaceDAO<Usuario> {
             statement.executeUpdate();
 
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Erro ao atualizar usuário.", e);
         }
-
     }
 
     @Override
     public void excluir(int id) {
 
-        String sql = "DELETE FROM usuario WHERE id=?";
+        String sql = "DELETE FROM usuario WHERE id = ?";
 
-        try (ConnectionFactory conn = conn.getConnectionFactory();
-            PreparedStatement statement = conn.prepareStatement(sql)) {
+        try (
+                Connection connection = ConnectionFactory.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql)
+        ) {
+
             statement.setInt(1, id);
             statement.executeUpdate();
-        } catch (SQLException e) {
-            throw new RuntimeException(e);
-        }
 
+        } catch (SQLException e) {
+            throw new RuntimeException("Erro ao excluir usuário.", e);
+        }
     }
 
     @Override
     public Usuario buscarPorId(int id) {
 
-        String sql = "SELECT * FROM usuario WHERE id=?";
+        String sql = "SELECT * FROM usuario WHERE id = ?";
 
         try (
-                ConnectionFactory conn = conn.getConnectionFactory();
-                PreparedStatement statement = conn.prepareStatement(sql)) {
+                Connection connection = ConnectionFactory.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql)
+        ) {
 
-                statement.setInt(1, id);
-                ResultSet res = statement.executeQuery();
+            statement.setInt(1, id);
 
-            if (res.next()) {
-                Usuario usuario = new Usuario();
+            try (ResultSet resultado = statement.executeQuery()) {
 
-                usuario.setId(res.getInt("id"));
-                usuario.setNome(res.getString("nome"));
-                usuario.setEmail(res.getString("email"));
-                usuario.setSenha(res.getString("senha"));
-                usuario.setDepartamento(departamentoDAO.buscarPorId(
-                            res.getInt("departamento_id")
-                    )
-                );
-
-                return usuario;
-
+                if (resultado.next()) {
+                    return criarUsuario(resultado);
+                }
             }
+
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Erro ao buscar usuário pelo ID.", e);
         }
 
         return null;
-
     }
 
     @Override
@@ -125,58 +123,62 @@ public class UsuarioDAO implements InterfaceDAO<Usuario> {
         String sql = "SELECT * FROM usuario ORDER BY nome";
 
         try (
-            ConnectionFactory conn = conn.getConnectionFactory();
-             Statement statement = conn.createStatement();
-             ResultSet res = statement.executeQuery(sql)) {
+                Connection connection = ConnectionFactory.getConnection();
+                Statement statement = connection.createStatement();
+                ResultSet resultado = statement.executeQuery(sql)
+        ) {
 
-            while (res.next()) {
-
-                Usuario usuario = new Usuario();
-                usuario.setId(res.getInt("id"));
-                usuario.setNome(res.getString("nome"));
-                usuario.setEmail(res.getString("email"));
-                usuario.setSenha(res.getString("senha"));
-                usuario.setDepartamento(departamentoDAO.buscarPorId(res.getInt("departamento_id")));
+            while (resultado.next()) {
+                Usuario usuario = criarUsuario(resultado);
                 usuarios.add(usuario);
             }
 
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Erro ao listar usuários.", e);
         }
 
         return usuarios;
-
     }
 
     public Usuario buscarPorEmail(String email) {
 
-        String sql = "SELECT * FROM usuario WHERE email=?";
+        String sql = "SELECT * FROM usuario WHERE email = ?";
 
-        try (ConnectionFactory conn = conn.getConnectionFactory();
-
-            PreparedStatement statement = conn.prepareStatement(sql)) {
+        try (
+                Connection connection = ConnectionFactory.getConnection();
+                PreparedStatement statement = connection.prepareStatement(sql)
+        ) {
 
             statement.setString(1, email);
-            ResultSet res = statement.executeQuery();
 
-            if (res.next()) {
-                Usuario usuario = new Usuario();
-                usuario.setId(res.getInt("id"));
-                usuario.setNome(res.getString("nome"));
-                usuario.setEmail(res.getString("email"));
-                usuario.setSenha(res.getString("senha"));
+            try (ResultSet resultado = statement.executeQuery()) {
 
-                usuario.setDepartamento(departamentoDAO.buscarPorId(res.getInt("departamento_id")));
-
-                return usuario;
+                if (resultado.next()) {
+                    return criarUsuario(resultado);
+                }
             }
 
         } catch (SQLException e) {
-            throw new RuntimeException(e);
+            throw new RuntimeException("Erro ao buscar usuário pelo e-mail.", e);
         }
 
         return null;
-
     }
 
-}
+    private Usuario criarUsuario(ResultSet resultado) throws SQLException {
+
+        Usuario usuario = new Usuario();
+
+        usuario.setId(resultado.getInt("id"));
+        usuario.setNome(resultado.getString("nome"));
+        usuario.setEmail(resultado.getString("email"));
+        usuario.setSenha(resultado.getString("senha"));
+
+        int departamentoId = resultado.getInt("departamento_id");
+
+        usuario.setDepartamento(
+                departamentoDAO.buscarPorId(departamentoId)
+        );
+
+        return usuario;
+    }
