116f4d6f41699d1d49da2a205bef2a2ccd031fd0
VitorOM2 <viktormarque2053@gmail.com>
2026-08-05 14:38:53 -0300

    Feature: Adiciona query para popular o banco

--- src/main/java/connection/DatabaseInitializer.java
+++ src/main/java/connection/DatabaseInitializer.java
@@ -10,6 +10,7 @@ import java.sql.Statement;
 public final class DatabaseInitializer {
 
     private static final String SQLITE_SCHEMA = "/database/sqlite-schema.sql";
+    private static final String SQLITE_DATA = "/database/sqlite-data.sql";
 
     private DatabaseInitializer() {
         throw new IllegalStateException("Classe utilitária não pode ser instanciada.");
@@ -26,6 +27,16 @@ public final class DatabaseInitializer {
                 Connection connection = ConnectionFactory.getConnection();
                 Statement statement = connection.createStatement()
         ) {
+            executarArquivoSql(
+                    statement,
+                    SQLITE_SCHEMA
+            );
+
+
+            executarArquivoSql(
+                    statement,
+                    SQLITE_DATA
+            );
             for (String comando : schema.split(";")) {
                 if (!comando.isBlank()) {
                     statement.execute(comando.trim());
@@ -49,5 +60,78 @@ public final class DatabaseInitializer {
         } catch (IOException e) {
             throw new RuntimeException("Erro ao ler o schema SQLite.", e);
         }
+    }
+
+    private static void executarArquivoSql(
+            Statement statement,
+            String arquivo
+    ) {
+
+        String sql = carregarArquivo(arquivo);
+
+
+        for (String comando : sql.split(";")) {
+
+            if (!comando.isBlank()) {
+
+                try {
+
+                    statement.execute(
+                            comando.trim()
+                    );
+
+                } catch (SQLException e) {
+
+                    throw new RuntimeException(
+                            "Erro executando comando SQL: "
+                                    + comando,
+                            e
+                    );
+
+                }
+
+            }
+
+        }
+
+    }
+
+
+    private static String carregarArquivo(
+            String caminho
+    ) {
+
+        try (
+                InputStream input =
+                        DatabaseInitializer.class
+                                .getResourceAsStream(caminho)
+        ) {
+
+
+            if (input == null) {
+
+                throw new IllegalStateException(
+                        "Arquivo SQL não encontrado: "
+                                + caminho
+                );
+
+            }
+
+
+            return new String(
+                    input.readAllBytes(),
+                    StandardCharsets.UTF_8
+            );
+
+
+        } catch (IOException e) {
+
+            throw new RuntimeException(
+                    "Erro ao ler arquivo SQL.",
+                    e
+            );
+
+        }
+
     }
 }
